317648adff229b0eeda886267d4c5cc071fa4c72
a-nakamura1015 <a-nakamura@esm.co.jp>
2019-02-05 17:29:23 +0900

    第1章を完了した

---
+++ src/chapter_1/Hello.java
@@ -0,0 +1,7 @@
+package chapter_1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/chapter_1/kadai/Kantan.java
@@ -0,0 +1,7 @@
+package chapter_1.kadai;
+
+public class Kantan {
+    public static void main(String[] args) {
+        System.out.println("意外と\"Java\"って簡単だ。\n意外と\"Java\"って簡単だ。\n意外と\"Java\"って簡単だ。");
+    }
+}
